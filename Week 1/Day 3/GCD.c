cb8b3abb98739fa95227c23d81959de56e9801a7
priyanshisingh09 <ps8799767@gmail.com>
2026-06-04 13:39:58 +0530

    Add files via upload

---
+++ Week 1/Day 3/GCD.c
@@ -0,0 +1,20 @@
+//Program to find GCD of two numbers using Euclidean algorithm
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    while (b != 0) {
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+
+    printf("GCD = %d\n", a);
+
+    return 0;
+}
