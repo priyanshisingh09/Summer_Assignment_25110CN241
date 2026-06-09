24a8609457f37256506f5959813084dc56376a10
priyanshisingh09 <ps8799767@gmail.com>
2026-06-09 22:41:41 +0530

    Add files via upload

---
+++ Day9/HollowSquare.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+int m,n;
+printf("enter the number of rows:");
+scanf("%d",&m);
+printf("enter the number of coloumns:");
+scanf("%d",&n);
+for(int i=1;i<=m;i++){
+    for(int j=1;j<=n;j++)
+    if(i==1||i==m ||j==1||j==n) printf("*");      
+    else printf(" ");
+    printf("\n");
+}
+return 0;
+}
