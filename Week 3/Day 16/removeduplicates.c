e326ff1b04714cf83f76dad5eeae80789cb666db
priyanshisingh09 <ps8799767@gmail.com>
2026-06-18 18:03:11 +0530

    Update removeduplicates.c

--- Week 3/Day 16/removeduplicates.c
+++ Week 3/Day 16/removeduplicates.c
@@ -5,11 +5,11 @@
 int main()
 {
     int n;
-
+    printf("Enter the number of element:");
     scanf("%d", &n);
 
     int arr[n];
-
+    printf("Enter the elements:");
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
