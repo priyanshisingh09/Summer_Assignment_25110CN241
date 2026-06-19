b258611d8de981f8548225fed491099d94aabdc3
priyanshisingh09 <ps8799767@gmail.com>
2026-06-19 21:51:40 +0530

    Add files via upload

---
+++ Week 3/Day 19/diagonalsumofmatrix.c
@@ -0,0 +1,25 @@
+// Program to Find Diagonal Sum of a Matrix
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, sum = 0;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++)
+        for(j = 0; j < n; j++)
+            scanf("%d", &a[i][j]);
+
+    for(i = 0; i < n; i++)
+        sum += a[i][i];
+
+    printf("Diagonal Sum = %d", sum);
+
+    return 0;
+}
