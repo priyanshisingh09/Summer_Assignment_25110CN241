96efce635b21c9d7f2f940db0b5f63d3fd2a983c
priyanshisingh09 <ps8799767@gmail.com>
2026-06-20 21:47:33 +0530

    Add files via upload

---
+++ Week 3/Day 20/symmetrymatrix.c
@@ -0,0 +1,41 @@
+// Write a program to Check Symmetric Matrix 
+
+#include <stdio.h>
+
+int main()
+{
+    int n, i, j, flag = 1;
+
+    printf("Enter order of square matrix: ");
+    scanf("%d", &n);
+
+    int a[n][n];
+
+    printf("Enter matrix elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(a[i][j] != a[j][i])
+            {
+                flag = 0;
+                break;
+            }
+        }
+    }
+
+    if(flag)
+        printf("Matrix is Symmetric");
+    else
+        printf("Matrix is Not Symmetric");
+
+    return 0;
+}
